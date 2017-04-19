e53897b3c5536c8c1de3c04053b795ba39b116ad
Alexis Ayala <alexis9613@hotmail.com>
2017-04-19 03:02:08 -0500

    js

---
+++ Alexis/LabWeb/js/piramide.js
@@ -0,0 +1,17 @@
+function generaPiramide() {
+	var p = document.getElementById("res");
+	p.innerHTML = '';
+	var n = document.getElementById("textbox").value;
+
+	if(n == ''){
+		n = 0;
+	}
+
+	for (var i = 1; i <= n; i++){
+		for (var j = 0; j < i; j++){
+			p.innerHTML +=0;
+		}
+		p.innerHTML += "<br>";
+
+	}
+}
