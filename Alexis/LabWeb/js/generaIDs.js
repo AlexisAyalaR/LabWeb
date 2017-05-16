ee402d484ba265ffac925b61a922dfb7b32a12a8
Alexis Ayala <alexis9613@hotmail.com>
2017-05-16 12:55:16 -0500

    ejercicio id

--- Alexis/LabWeb/js/generaIDs.js
+++ Alexis/LabWeb/js/generaIDs.js
@@ -1,30 +1,62 @@
-var n = 1;
 
 function generaIdentidad(){
 
+	var n = Math.floor((Math.random() * 100000) + 1);
 	var p = document.getElementById("res");
 	var div = generaDiv();
-	var img = document.createElement("img");
-	var id = document.createElement("p");
+	var divImg = generaDivImg();
+	var img = document.createElement("IMG");
+	var id = document.createElement("P");
 	var t = document.createTextNode(n);
+	var x = generaClose(div);
 	img.src = "../../img/lol.png";
-	img.height = "150";
+	img.height = "140";
 	img.width = "100";
-	div.appendChild(img);
+	divImg.appendChild(img);
 	id.appendChild(t);
+	div.appendChild(x);
+	div.appendChild(divImg);
 	div.appendChild(id);
 	p.appendChild(div);
-	n = n + 1;
+	x.addEventListener("click", function(){ div.parentNode.removeChild(div); });
 }
 
 function generaDiv(){
-	var div = document.createElement("div");
+	var div = document.createElement("DIV");
 	div.style.width = "150";
 	div.style.height = "210";
 	div.style.cssFloat = "left";
 	div.style.textAlign = "center";
 	div.style.border = "3px solid white";
-	div.style.backgroundColor = "#556DF0"
+	div.style.borderRadius = "15px 0px 30px";
+	div.style.backgroundColor = "#0073e6";
 	div.style.color = "white";
 	return div;
 }
+
+function generaDivImg(){
+	var divImg = document.createElement("DIV");
+	divImg.style.width = "120";
+	divImg.style.height = "145";
+	divImg.style.backgroundColor = "#0080ff";
+	divImg.style.textAlign = "center";
+	divImg.style.marginTop = "20";
+	divImg.style.marginLeft = "15";
+	divImg.style.border = "2px solid #0073e6";
+	divImg.style.borderRadius = "10px";
+	return divImg;
+}
+
+function generaClose(div){
+	var x = document.createElement("BUTTON");
+	var xClose = document.createTextNode("x");
+	x.appendChild(xClose);
+	x.style.cssFloat = "right";
+	x.style.marginRight = "3";
+	x.style.marginTop = "3";
+	return x;
+}
+
+
+
+		//{ div.parentNode.removeChild(div); });
